6ea57a3d4f1af2be0b7c8423a1ebbe55864a0f1a
Abdullah Binsaad <abdullah.binsaadd@gmail.com>
2023-05-04 23:59:35 +0300

    Completed Assignment

---
+++ src/components/Article.jsx
@@ -0,0 +1,10 @@
+const Articale = (props) => {
+    return ( 
+        <article>
+            <h3>{props.title}</h3>
+            <small>{props.date || "January 1, 1970"}</small>
+            <p>{props.preview}</p>
+        </article>
+    );
+}
+    export default Articale
